c4736b1a8a2361501ad4edbe9b01932beaf5fb43
dslimm <157145338+dslimm@users.noreply.github.com>
2024-06-12 13:46:27 +0300

    Update arrLevel.js

--- settings/arrLevel.js
+++ settings/arrLevel.js
@@ -11,7 +11,7 @@ const levelsArr = [
     {
         id: "1",
         value: "2, 3, 5",
-        target: "100",
+        target: "10",
         createFixCub: [
             {
                 fixCubCard: "1",
@@ -30,7 +30,7 @@ const levelsArr = [
     {
         id: "2",
         value: "1, 4, 5, 10, ✘",
-        target: "200",
+        target: "20",
         startTimerGame: "0",
         getRandomValue: "1",
         gameProcess: "1",
@@ -39,7 +39,7 @@ const levelsArr = [
     {
         id: "3",
         value: "2, 3, 5, 10",
-        target: "200",
+        target: "20",
         startTimerGame: "0",
         getRandomValue: "7",
         gameProcess: "1",
@@ -48,7 +48,7 @@ const levelsArr = [
     {
         id: "4",
         value: "1, 2, 5, 10",
-        target: "300",
+        target: "30",
         startTimerGame: "0",
         getRandomValue: "4",
         gameProcess: "4",
@@ -58,7 +58,7 @@ const levelsArr = [
     {
         id: "5",
         value: "3, 4, 5, 10",
-        target: "300",
+        target: "30",
         startTimerGame: "0",
         getRandomValue: "5",
         gameProcess: "5",
@@ -68,7 +68,7 @@ const levelsArr = [
     {
         id: "6",
         value: "2, 3, 10",
-        target: "400",
+        target: "10",
         createSingleCub: [
             {
                 singleCubCard: "9",
@@ -83,7 +83,7 @@ const levelsArr = [
     {
         id: "7",
         value: "2, 4, 5, 10, ⇆ ",
-        target: "400",
+        target: "10",
         startTimerGame: "0",
         getRandomValue: "2",
         gameProcess: "2",
@@ -92,7 +92,7 @@ const levelsArr = [
     {
         id: "8",
         value: "1, 4, 10",
-        target: "500",
+        target: "10",
         createSingleCub: [
             {
                 singleCubCard: "8",
@@ -106,11 +106,12 @@ const levelsArr = [
         startTimerGame: "0",
         getRandomValue: "0",
         gameProcess: "0",
+        calculateScore: "0",
     },
     {
         id: "9",
         value: "0, 2, 3, 4, 5, 10",
-        target: "500",
+        target: "10",
         startTimerGame: "0",
         getRandomValue: "6",
         gameProcess: "0",
@@ -119,10 +120,120 @@ const levelsArr = [
     {
         id: "10",
         value: "2, 4, 6, 10, ⤋",
-        target: "500",
+        target: "10",
         startTimerGame: "0",
         getRandomValue: "3",
         gameProcess: "3",
+        calculateScore: "0",
+    },
+    {
+        id: "11",
+    },
+    {
+        id: "12",
+    },
+    {
+        id: "13",
+    },
+    {
+        id: "14",
+    },
+    {
+        id: "15",
+    },
+    {
+        id: "16",
+    },
+    {
+        id: "17",
+    },
+    {
+        id: "18",
+    },
+    {
+        id: "19",
+    },
+    {
+        id: "20",
+    },
+    {
+        id: "21",
+    },
+    {
+        id: "22",
+    },
+    {
+        id: "23",
+    },
+    {
+        id: "24",
+    },
+    {
+        id: "25",
+    },
+    {
+        id: "26",
+    },
+    {
+        id: "27",
+    },
+    {
+        id: "28",
+    },
+    {
+        id: "29",
+    },
+    {
+        id: "30",
+    },
+    {
+        id: "31",
+    },
+    {
+        id: "32",
+    },
+    {
+        id: "33",
+    },
+    {
+        id: "34",
+    },
+    {
+        id: "35",
+    },
+    {
+        id: "36",
+    },
+    {
+        id: "37",
+    },
+    {
+        id: "38",
+    },
+    {
+        id: "39",
+    },
+    {
+        id: "40",
+    },
+
+    {
+        id: "101",
+        value: "2, 3, 5",
+        target: "10",
+        createFixCub: [
+            {
+                fixCubCard: "1",
+                fixCubValue: "55",
+            },
+            {
+                fixCubCard: "9",
+                fixCubValue: "20",
+            },
+        ],
+        startTimerGame: "0",
+        getRandomValue: "0",
+        gameProcess: "0",
         calculateScore: "0",
     },
 ];
